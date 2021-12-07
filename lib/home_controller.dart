6877aff410fd1972ff0c412913a248536e771f09
Electro <75930721+Electro-Magnet45@users.noreply.github.com>
2021-12-07 20:45:03 +0530

    Door Lock Commit

---
+++ lib/home_controller.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class HomeController extends ChangeNotifier {
+  bool isRightDoorLock = true;
+  bool isLeftDoorLock = true;
+  bool isBonnetLock = true;
+  bool isTrunkLock = true;
+
+  void updateRightDoorLock() {
+    isRightDoorLock = !isRightDoorLock;
+    //something like setState(){}
+    notifyListeners();
+  }
+
+  void updateLeftDoorLock() {
+    isLeftDoorLock = !isLeftDoorLock;
+    notifyListeners();
+  }
+
+  void updateBonnetDoorLock() {
+    isBonnetLock = !isBonnetLock;
+    notifyListeners();
+  }
+
+  void updateTrunkDoorLock() {
+    isTrunkLock = !isTrunkLock;
+    notifyListeners();
+  }
+}
